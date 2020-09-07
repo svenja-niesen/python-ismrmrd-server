b7ae07a5a2fcb61a7e33d70b61899efc21245871
Philipp Ehses <philipp.ehses@dzne.de>
2020-09-07 15:56:29 +0200

    set bart's pythonpath in FIRE sh scripts

--- start-fire-python-server.sh
+++ start-fire-python-server.sh
@@ -3,6 +3,9 @@
 # Bash script to start Python ISMRMRD server.  First argument is path to log file.
 # If no argument is provided, logging is done to stdout (and discarded)
 
+# set pythonpath for bart (docker's ENV or export in /root/.bashrc do not seem to work)
+export PYTHONPATH=${PYTHONPATH}:/opt/code/bart/python
+
 if [ $# -eq 1 ]; then
   LOG_FILE=${1}
   /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
