bug fix for masking the adc maps

powergrid_pulseq.py
@@ -304,7 +304,7 @@ def process_raw(acqGroup, metadata, sensmaps, prot_arrays, slc_sel=None):
     # Comment from Alex Cerjanic, who built PowerGrid: 'histo' option can generate a bad set of interpolators in edge cases
     # He recommends using the Hanning interpolator with ~1 time segment per ms of readout (which is based on experience @3T)
     # However, histo lead to quite nice results so far & does not need as many time segments
-    data = PowerGridIsmrmrd(inFile=tmp_file, outFile=debug_pg+"/img", timesegs=25, niter=10, nShots=n_shots, beta=0, 
+    data = PowerGridIsmrmrd(inFile=tmp_file, outFile=debug_pg+"/img", timesegs=20, niter=10, nShots=n_shots, beta=0, 
                                 ts_adapt=False, TSInterp='hanning', FourierTrans='NUFFT')
     shapes = data["shapes"] 
     data = np.asarray(data["img_data"]).reshape(shapes)
@@ -436,7 +436,7 @@ def process_diffusion_images(b0, diffw_imgs, prot_arrays, mask):
     else:
         adc_map = np.polynomial.polynomial.polyfit(b_val[1:], trace_log.reshape([-1,n_bval]).T, 1)[1,].T.reshape(imgshape)
 
-    adc_map *= mask
+    adc_map = adc_map * mask
 
     return adc_map
 